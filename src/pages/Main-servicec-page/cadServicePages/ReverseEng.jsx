f6c8ab7966fe78b3cb483ebd66b1f651046e69a1
ishu rajput <ishwarrajput143.com>
2024-04-11 21:09:35 +0530

    Reverse Eng page

--- src/pages/Main-servicec-page/cadServicePages/ReverseEng.jsx
+++ src/pages/Main-servicec-page/cadServicePages/ReverseEng.jsx
@@ -6,6 +6,7 @@ import Img4 from "../../../asset/main-services-page/cmd-service/Reverse-Eng/img-
 import Img5 from "../../../asset/main-services-page/cmd-service/Reverse-Eng/img-5.png"
 import Img6 from "../../../asset/main-services-page/cmd-service/Reverse-Eng/img-6.png"
 import Img7 from "../../../asset/main-services-page/cmd-service/Reverse-Eng/img-7.png"
+import Img8 from "../../../asset/main-services-page/cmd-service/Reverse-Eng/img-8.png"
 import Navbar from '../../../component/Navbar'
 import Footer from '../../../component/Footer'
 
@@ -13,8 +14,8 @@ const ReverseEng = () => {
 
     return <div>
         {/* <Navbar /> */}
-        <div className='bg-black text-white py-10 px-8 '>
-            <div className=" grid grid-cols-12  gap-4 py-16">
+        <div className='  py-10 px-8 '>
+            <div className="bg-black text-white grid grid-cols-12  gap-4 py-16">
                 <div className=" col-span-12 md:col-span-6 lg:col-span-6  p-4">
                     <img src={Img1} alt="" className=" object-fill " />
                 </div>
@@ -30,9 +31,37 @@ const ReverseEng = () => {
                 </div>
 
             </div>
+        </div>
+
+        <div className='grid grid-cols-12 gap-1 mb-20 mt-10'>
+            <div className='md:col-span-5 col-span-12 md:border-e md:ps-0 mb-10 ps-10 border-slate-500 md:relative '>
+                <div className='md:-rotate-90 md:absolute h-full font-bold md:-right-60 text-4xl'>
+                    <p className='text-red-600'>Why</p>
+                    <p className='text-slate-800'> Reverse Engineering?</p>
+                </div>
+            </div>
+            <div className='md:col-span-7 col-span-12 md:border-s-2 border-slate-500  px-10'>
+                <ul className="list-disc list-inside space-y-4 text-2xl text-slate-900">
+                    <li className=''>Enhances product competitiveness</li>
+                    <div className='border-b-[1px] w-44 border-slate-300 '></div>
+                    <li className=''>Design and generate cad models</li>
+                    <div className='border-b-[1px] w-44 border-slate-300 '></div>
+                    <li className=''>Enhances product competitiveness</li>
+                    <div className='border-b-[1px] w-44 border-slate-300 '></div>
+                    <li className=''>Inspiring Innovation</li>
+                    <div className='border-b-[1px] w-44 border-slate-300 '></div>
+                    <li className=''>Repairing existing products</li>
+                    <div className='border-b-[1px] w-44 border-slate-300 '></div>
+                    <li className=''>Vulnerability discovery</li>
+                    <div className='border-b-[1px] w-44 border-slate-300 '></div>
+                </ul>
+
+            </div>
+
+        </div>
 
 
-            <div className='text-center flex-col flex items-center mt-28'>
+        {/* <div className='text-center flex-col flex items-center mt-28'>
                 <div className='text-2xl font-bold '>
                     Why Reverse Engineering?
                 </div>
@@ -87,30 +116,57 @@ const ReverseEng = () => {
                     </div>
                 </div>
 
+            </div> */}
+
+
+        <div className=" grid grid-cols-12 mt-10 text-black mx-6">
+
+            <div className=" col-span-12 md:col-span-6 lg:col-span-6 p-4 flex items-center ">
+                <img src={Img2} alt="" className=" object-cover lg:h-[20rem] h-full w-full  " />
             </div>
 
-        </div>
-
-        <div className=" grid grid-cols-12 bg-black  text-black">
-
-
-            <div className="col-span-12 bg-white md:col-span-6 lg:col-span-6 p-10 ps-14">
-                <div className="md:text-4xl text-3xl font-bold "> 3D Scan to CAD model</div>
-                <div className='border-2 rounded-xl w-48 mt-3 border-pink-600'></div>
+            <div className="col-span-12 bg-white md:col-span-6 lg:col-span-6 p-4 ">
+                <div className="md:text-4xl text-3xl font-semibold ">3D Scan to CAD model</div>
+                <div className='border-2 rounded-xl w-48 mt-3 border-red-600'></div>
 
                 <div className=" pt-6">
                     You want to reverse engineer a geometric shaped part? Simply export your mesh to Reverse Engineering, then start to create, combine, extract and adjust. As you’re in control of the entire process you get exactly the result you want.Reverse Engineering offers a range of functions to help you achieve the highest accuracy for free form surfaces. For instance: To convert your mesh, choose “Surface Approximation to Points” then the software guides you to your mold.Take your designs to the next dimension with our parametric 3D modeling services. We create 3D models for parts and assemblies that are not only visually stunning but also highly functional.
                 </div>
 
             </div>
-            <div className=" col-span-12 md:col-span-6 lg:col-span-6 bg-black">
-                <img src={Img2} alt="" className=" object-cover h-[30rem]" />
-            </div>
+
         </div>
-        <div className='bg-black text-white p-10 pt-32  '>
+        <div className='mx-10 my-14'>
+            <div className='text-3xl my-4'>
+                <p className='font-semibold'>How do We work?</p>
+                <div className='border-2 border-red-600 w-60 mt-2'></div>
+            </div>
+
+            <div className='grid grid-cols-12  gap-4'>
+                <div className='md:col-span-6 col-span-12'>
+                    <p>
+                        You want to reverse engineer a geometric shaped part? Simply export your mesh to Reverse Engineering, then start to create, combine, extract and adjust. As you’re in control of the entire process you get exactly the result you want.Reverse Engineering offers a range of functions to help you achieve the highest accuracy for free form surfaces.
+                    </p>
+                    <p className='hidden lg:block'>
+                        For instance: To convert your mesh, choose “Surface Approximation to Points” then the software guides you to your mold.Take your designs to the next dimension with our parametric 3D modeling services. We create 3D models. You want to reverse engineer a geometric shaped part? Simply export your mesh to Reverse Engineering, then start to create, combine, extract and adjust. As
+                        You want to reverse engineer a Simp. You want to reverse engineer a geometric shaped part? SimpYou want to reverse engineer a geometric shaped part? SimpYou want to reverse engineer a geometric shaped part?
+                    </p>
+                </div>
+                <div className='md:col-span-6 col-span-12'>
+                    <img src={Img8} alt="" className='object-fill h-full' />
+                </div>
+
+            </div>
+            <p className='mt-1'>
+                You want to reverse engineer a geometric shaped part? Simply export your mesh to Reverse Engineering, then start to create, combine, extract and adjust. As you’re in control of the entire process you get exactly the result you want.Reverse Engineering offers a range of functions to help you achieve the highest accuracy for free form surfaces. For instance: To convert your mesh, choose “Surface Approximation to Points” then the software guides you to your mold.Take your designs to the next dimension with our parametric 3D modeling services. We create 3D models.
+                You want to reverse engineer a geometric shaped part? Simp. You want to reverse engineer a geometric shaped part? Simply export your mesh to Reverse Engineering, then start to create, combine, extract and adjust. As you’re in control of the entire process you get exactly the result you want.Reverse Engineering offers a range of functions to help you achieve the highest accuracy for free form surfaces. For instance: To convert your mesh, choose “Surface Approximation to Points” then the software guides you to your mold.Take your designs to the next dimension with our parametric 3D modeling services. We create 3D models.. You want to reverse engineer a geometric shaped part? Simply export your mesh to Reverse Engineering, then start to create, combine, extract and adjust.
+            </p>
+        </div>
+
+        {/* <div className='bg-black text-white p-10 pt-32  '>
 
             <div className='text-center flex-col flex items-center '>
-                <div className='text-2xl font-bold '>
+                <div className='md:text-4xl text-3xl font-bold '>
                     From 3D scan data to CAD model
                 </div>
                 <div className='border-pink-600 w-48 mt-3 border-2 rounded-xl'></div>
@@ -155,10 +211,10 @@ const ReverseEng = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </div> */}
 
 
-        <div className='py-10 px-8 '>
+        {/* <div className='py-10 px-8 '>
             <div class="grid grid-cols-12 gap-6 mt-6">
                 <div class="lg:col-start-3 lg:col-span-4 md:col-span-6 col-span-12 ...">
                     <img src={Img6} alt="" className="w-full" />
@@ -187,7 +243,8 @@ const ReverseEng = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </div> */}
+
         {/* <Footer /> */}
     </div>
 }
